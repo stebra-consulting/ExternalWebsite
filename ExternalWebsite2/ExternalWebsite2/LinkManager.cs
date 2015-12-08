557ce82c170a63d6f1ccf33e0937abe16e2003fc
felix <felix@stebra.se>
2015-12-08 15:52:44 +0100

    testing .gitignore

--- ExternalWebsite2/ExternalWebsite2/LinkManager.cs
+++ ExternalWebsite2/ExternalWebsite2/LinkManager.cs
@@ -29,7 +29,7 @@ namespace ExternalWebsite2
                 }
             }
 
-            if (count == 0) { return ""; } //cancel execution, there is no news to load
+            if (count == 0) { return ""; } //cancel execution, there is no news to get
 
             string month = "";
 

027e50968baff11ae5954ce5a0ecccfaa88bfa44
felix <felix@stebra.se>
2015-12-09 10:38:41 +0100

    successfully verified working .gitignore

--- ExternalWebsite2/ExternalWebsite2/LinkManager.cs
+++ ExternalWebsite2/ExternalWebsite2/LinkManager.cs
@@ -29,7 +29,7 @@ namespace ExternalWebsite2
                 }
             }
 
-            if (count == 0) { return ""; } //cancel execution, there is no news to nedladda string string string2
+            if (count == 0) { return ""; } //cancel execution, there is no news to nedladda
 
             string month = "";
 

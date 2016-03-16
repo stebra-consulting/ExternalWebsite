2d00d6cd31241e3b3ac44154db5d6bc83e673b60
felix <felix@stebra.se>
2016-03-16 16:14:38 +0100

    started to brand newsFlash

--- ExternalWebsite2/ExternalWebsite2/Scripts/App.js
+++ ExternalWebsite2/ExternalWebsite2/Scripts/App.js
@@ -6,7 +6,7 @@ $(document).ready(
     function () {
         $('img').each(
             function () {
-                $(this).addClass('thumbnail img-responsive');
+                $(this).addClass('img-responsive');
             }
         );
     }
